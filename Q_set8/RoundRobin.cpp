c859f36d9557d5dbe8bea78609773e1d53786149
Akash Chouhan <akash.c1500@gmail.com>
2021-03-13 22:53:36 +0530

    added Q on PreemptiveAlgo

---
+++ Q_set8/RoundRobin.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+void findWaitingTime(int processes[], int n, int bt[], int wt[], int quantum)
+{
+    int rem_bt[n];
+    for (int i = 0; i < n; i++)
+        rem_bt[i] = bt[i];
+
+    int t = 0; // Current time
+
+    while (1)
+    {
+        bool done = true;
+        for (int i = 0; i < n; i++)
+        {
+            if (rem_bt[i] > 0)
+            {
+                done = false;
+
+                if (rem_bt[i] > quantum)
+                {
+                    t += quantum;
+                    rem_bt[i] -= quantum;
+                }
+                else
+                {
+                    t = t + rem_bt[i];
+                    wt[i] = t - bt[i];
+                    rem_bt[i] = 0;
+                }
+            }
+        }
+        if (done == true)
+            break;
+    }
+}
+
+// Function to calculate turn around time
+void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[])
+{
+    for (int i = 0; i < n; i++)
+        tat[i] = bt[i] + wt[i];
+}
+
+// Function to calculate average time
+void findavgTime(int processes[], int n, int bt[], int quantum)
+{
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+
+    findWaitingTime(processes, n, bt, wt, quantum);
+    findTurnAroundTime(processes, n, bt, wt, tat);
+    cout << "Processes |"
+         << " Burst time |"
+         << " Waiting time |"
+         << " Turn around time |\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        total_wt = total_wt + wt[i];
+        total_tat = total_tat + tat[i];
+        cout << " P" << i + 1 << "\t\t" << bt[i] << "\t "
+             << wt[i] << "\t\t " << tat[i] << endl;
+    }
+
+    cout << "Average waiting time = "
+         << (float)total_wt / (float)n;
+    cout << "\nAverage turn around time = "
+         << (float)total_tat / (float)n;
+}
+
+// Driver code
+int main()
+{
+    int n;
+    cout << "Enter the number of processes : ";
+    cin >> n;
+    int processes[n] = {0};
+    int quantum = 1, burst_time[n] = {0};
+    cout << "Enter the Burst Times for the " << n << " process : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> burst_time[i];
+    }
+    cout << "Enter the Time Quantum : ";
+
+    cin >> quantum;
+
+    findavgTime(processes, n, burst_time, quantum);
+    return 0;
+}
